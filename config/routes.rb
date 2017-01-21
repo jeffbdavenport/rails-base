6f66e4648357c7760cfb19fb831bba5574a27d18
Jeffrey Davenport <jeffreydvp@gmail.com>
2017-01-21 12:01:39 -0700

    Removed Administrate

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   namespace :admin do
-    
-    root to: "#index"
+    root to: '#index'
   end
 
   get 'welcome/index'
