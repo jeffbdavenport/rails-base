552a9a6284868aff984a6051c28a8c40d3277114
Jeffrey Davenport <jeffreydvp@gmail.com>
2017-01-21 20:43:18 -0700

    Switch to development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -4,7 +4,7 @@ Rails.application.configure do
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
-  config.console.whitelisted_ips = ENV['MY_IP']
+  # config.console.whitelisted_ips = ENV['MY_IP']
   config.cache_classes = false
 
   # Do not eager load code on boot.
